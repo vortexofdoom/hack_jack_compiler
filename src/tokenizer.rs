b26d4731906766d082e487ff50fde06e37dc2435
vortexofdoom97@gmail.com <vortexofdoom97@gmail.com>
2022-11-24 17:43:30 -0600

    Basic tokenizer control flow complete

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,19 +1,21 @@
 use crate::tokens::*;
-use std::error::Error;
 
-fn create_token<T>(data: T) -> Result<Token, Box<dyn Error>>
-where T: char + str {
-    let n = u16::try_from(data)
-    Ok(Token::IntConst(0))
+pub enum TokenizerError {
+    InvalidInt,
+    UnrecognizedToken,
 }
 
-pub fn parse(code: String) -> Result<Vec<Token>, Box<dyn Error>> {
+impl From<std::num::ParseIntError> for TokenizerError {
+    fn from(_: std::num::ParseIntError) -> Self {
+        TokenizerError::InvalidInt
+    }
+}
+
+pub fn parse(code: String) -> Result<Vec<Token>, TokenizerError> {
     let mut in_string = false;
-    let mut in_word = false;
     let mut in_comment = false;
     let mut multiline_comment = false;
     let mut str_start = 0;
-    let mut word_start = 0;
     let mut tokens = vec![];
     let mut chars = code.chars().enumerate().peekable();
     while let Some((i, c)) = chars.next() {
@@ -32,61 +34,49 @@ pub fn parse(code: String) -> Result<Vec<Token>, Box<dyn Error>> {
             }
         } else if in_string {
             if c == '"' {
-                tokens.push(create_token(&code[str_start..i])?);
+                tokens.push(Token::StringConst(String::from(&code[str_start..i])));
                 in_string = false;
             }
         } else {
-            if c.is_numeric() && !in_word {
+            if c.is_numeric() {
                 let start = i;
                 let mut end = start;
                 while let Some((j, _)) = chars.next_if(|(_, x)| x.is_numeric()) {
                     end = j;
                 }
-                tokens.push(create_token(&code[start..=end])?);
-            } else if c.is_alphabetic() && !in_word {
-                (in_word, word_start) = (true, i + 1);
-            } else {
-                match c {
-                    '"' => {
-                        if !in_string {
-                            (in_string, str_start) = (true, i + 1);
-                        }
-                    }
-                    '/' => {
-                        if !in_comment && !in_string {
-                            if let Some((_, '/')) | Some((_, '*')) = chars.peek() {
-                                in_comment = true;
-                                multiline_comment = chars.next().unwrap().1 == '*'; 
-                            } else {
-                                tokens.push(create_token('/')?);
-                            }
-                        }
-                    }
-                    '*' => {
-                        if in_comment && multiline_comment {
-                            if let Some(_) = chars.next_if_eq(&(i + 1, '/')) {
-                                (in_comment, multiline_comment) = (false, false);
-                            }
-                        } else {
-                            tokens.push(create_token(c)?);
-                        }
-                    }
-                    ' ' => {
-                        if in_word {
-                            in_word = false;
-                            let word = &code[word_start..i];
-                            if let Some(k) = KEYWORDS.get(word) {
-                                tokens.push(Token::Keyword(*k));
-                            }
-                        }
-                    }
-                    '\n' => {}
-                    _ => {
-                        if SYMBOLS.contains(&c) {
-                            tokens.push(Token::Symbol(c));
-                        }
-                    },
+                let n = (&code[start..=end]).parse::<i16>()?;
+                tokens.push(Token::IntConst(n));
+            } else if c.is_alphabetic() {
+                let start = i;
+                let mut end = start;
+                while let Some((j, _)) = chars.next_if(|(_, x)| x.is_alphanumeric()) {
+                    end = j;
                 }
+                let word = &code[start..=end];
+                if let Some(k) = KEYWORDS.get(word){
+                    tokens.push(Token::Keyword(*k));
+                } else {
+                    tokens.push(Token::Identifier(String::from(word)));
+                }
+            } else if SYMBOLS.contains(&c) {
+                if c == '/' {
+                    if !in_comment && !in_string {
+                        if let Some((_, '/')) | Some((_, '*')) = chars.peek() {
+                            in_comment = true;
+                            multiline_comment = chars.next().unwrap().1 == '*'; 
+                        } else {
+                            tokens.push(Token::Symbol('/'));
+                        }
+                    }
+                } else if c == '"' {
+                    if !in_string {
+                        (in_string, str_start) = (true, i + 1);
+                    }
+                } else {
+                    tokens.push(Token::Symbol(c));
+                }
+            } else {
+                return Err(TokenizerError::UnrecognizedToken);
             }
         }
     }
